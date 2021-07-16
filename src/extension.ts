2dadb17bbd6a616b8d5ec90dabd75ccbb8bcbf4b
Fabio Spampinato <spampinabio@gmail.com>
2021-07-16 17:57:08 +0100

    Added a dialog announcing the fundraising

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 /* IMPORT */
 
 import * as vscode from 'vscode';
+import beggar from 'vscode-beggar';
 import './statusbar';
 import Config from './config';
 import Utils from './utils';
@@ -9,6 +10,23 @@ import Utils from './utils';
 /* ACTIVATE */
 
 async function activate ( context: vscode.ExtensionContext ) {
+
+  beggar ({
+    id: 'vscode-projects-plus',
+    title: '𝗣𝗿𝗼𝗷𝗲𝗰𝘁𝘀+ - 𝗙𝘂𝗻𝗱𝗿𝗮𝗶𝘀𝗶𝗻𝗴 𝗔𝗻𝗻𝗼𝘂𝗻𝗰𝗲𝗺𝗲𝗻𝘁: We are collecting some money to allow for further development, if you find this extension useful please please please consider donating to it and be part of something amazing!',
+    url: 'https://buy.stripe.com/3cseWHeAp1H1dVe14w',
+    actions: {
+      yes: {
+        webhook: `https://telemetry.notable.app/track?events=%5B%7B%22event%22%3A%22vscode-beggar%22%2C%22extension%22%3A%22vscode-projects-plus%22%2C%22result%22%3A1%2C%22timestamp%22%3A${Date.now ()}%7D%5D`
+      },
+      no: {
+        webhook: `https://telemetry.notable.app/track?events=%5B%7B%22event%22%3A%22vscode-beggar%22%2C%22extension%22%3A%22vscode-projects-plus%22%2C%22result%22%3A0%2C%22timestamp%22%3A${Date.now ()}%7D%5D`
+      },
+      cancel: {
+        webhook: `https://telemetry.notable.app/track?events=%5B%7B%22event%22%3A%22vscode-beggar%22%2C%22extension%22%3A%22vscode-projects-plus%22%2C%22result%22%3A2%2C%22timestamp%22%3A${Date.now ()}%7D%5D`
+      }
+    }
+  });
 
   const config = await Config.get ();
 
